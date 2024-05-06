180a983bcc4c3a9771e61e4eac4cb686bc25e8a9
Tomek <tomaszmakowski002731@gmail.com>
2024-05-06 23:46:35 +0200

    Add extra imgs counter

--- gallery.js
+++ gallery.js
@@ -1,3 +1,8 @@
+const imgCountEl = document.getElementById("img-count");
+const hiddenImgEls = document.querySelectorAll("#galeria .hidden a");
+
+imgCountEl.textContent = hiddenImgEls.length;
+
 lightGallery(document.getElementById('galeria'), {
   plugins: [lgZoom, lgThumbnail],
   selector: "a",
